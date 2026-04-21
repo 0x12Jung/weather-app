ec3462cce700d3ae75d2309405905fd256c5e239
Jung <0x12jung@gmail.com>
2026-04-21 13:44:38 +0800

    Refactor add city flow to state-driven UI

--- feature/weather/src/main/kotlin/com/opnt/takehometest/feature/weather/addcity/AddCityUiState.kt
+++ feature/weather/src/main/kotlin/com/opnt/takehometest/feature/weather/addcity/AddCityUiState.kt
@@ -2,19 +2,25 @@ package com.opnt.takehometest.feature.weather.addcity
 
 import com.opnt.takehometest.core.domain.model.City
 
-sealed interface AddCityUiState {
-    data object Idle : AddCityUiState
-    data object Loading : AddCityUiState
-    data class Results(val cities: List<AddCityResultItem>) : AddCityUiState
-    data object NoResults : AddCityUiState
-    data class Error(val error: AddCityError) : AddCityUiState
+data class AddCityUiState(
+    val searchState: AddCitySearchState = AddCitySearchState.Idle,
+    val showAddFailedMsg: Boolean = false,
+    val isAdded: Boolean = false,
+)
+
+sealed interface AddCitySearchState {
+    data object Idle : AddCitySearchState
+    data object Loading : AddCitySearchState
+    data class Results(val cities: List<AddCityResultItem>) : AddCitySearchState
+    data object NoResults : AddCitySearchState
+    data class Error(val error: AddCityError) : AddCitySearchState
 }
 
 enum class AddCityError { SearchFailed }
 
 data class AddCityResultItem(val city: City, val alreadySaved: Boolean)
 
-sealed interface AddCityEvent {
-    data object CityAdded : AddCityEvent
-    data object AddFailed : AddCityEvent
-}
+data class AddCityTransientState(
+    val showAddFailedMsg: Boolean = false,
+    val isAdded: Boolean = false,
+)
